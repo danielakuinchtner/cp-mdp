eb015be79205e90fa582098256c700a5a6b3fcd5
danielakuinchtner <danielakuinchtner@gmail.com>
2020-09-14 20:52:07 -0300

    n-dimensional shape

---
+++ gridworld.py
@@ -0,0 +1,168 @@
+# The line below is to be used if you have pymdptoolbox installed with setuptools
+# import mdptoolbox.example
+# Whereas the line below obviate the need to install that
+import sys
+sys.path.insert(1,'pymdptoolbox/src')
+import mdptoolbox.example
+import time
+from gridworld_scenario import *
+
+"""
+(Y,X)
+| 00 01 02 ... 0X-1       'N' = North
+| 10  .         .         'S' = South
+| 20    .       .         'W' = West
+| .       .     .         'E' = East
+| .         .   .         'T' = Terminal
+| .           . .         'O' = Obstacle
+| Y-1,0 . . .   Y-1X-1
+"""
+
+# install openblas lib to improve even more the runtime: conda install -c anaconda openblas
+
+shape = [3, 4, 2]
+dimensions = len(shape)
+print('Number of dimensions: ', dimensions)
+rewards = [[0, 3, 100], [1, 3, -100]]
+obstacles = [[1, 1]]
+terminals = [[0, 3], [1, 3]]
+actions = ['N', 'E', 'W', 'S']
+final_limits = [shape[0]-1, shape[1]-1]
+
+states = 1
+for value in shape:
+    states *= value
+
+#states = shape[0] * shape[1]
+print("Executing a", shape, "grid")
+
+start_time1 = time.time()
+succ_xy, origin_xy, probability_xy, R = mdp_grid(shape=shape, terminals=terminals, r=-3,
+                                                                         rewards=rewards, obstacles=obstacles,
+                                                                         actions=actions, final_limits=final_limits)
+
+vi = mdptoolbox.mdp.ValueIterationGS(shape, terminals, obstacles, succ_xy, origin_xy, probability_xy, R, states,
+                                     discount=1, epsilon=0.001, max_iter=1000, skip_check=True)
+
+vi.run()
+
+print("--- %s seconds ---" % (time.time() - start_time1))
+
+# print_policy(vi.policy, shape, obstacles=obstacles, terminals=terminals, actions=actions)
+# display_policy(vi.policy, shape, obstacles=obstacles, terminals=terminals)
+
+
+# print(output)
+# print(R)
+
+# ----------------------------------------------
+# RESULTS OF MY ALGORITHM
+# ----------------------------------------------
+
+# 3x4
+# --- 0.014991998672485352 seconds ---
+# --- 0.015987873077392578 seconds ---
+# --- 0.013991594314575195 seconds ---
+# --------------- 0,0149 average
+
+# 10x10
+# --- 0.17789649963378906 seconds ---
+# --- 0.21387767791748047 seconds ---
+# --- 0.21387767791748047 seconds ---
+# --------------- 0,1861 average
+
+# 30x30
+# --- 2.0977985858917236 seconds --- diminuiu 50% do tempo
+
+# 40x40
+# --- 5.280977487564087 seconds --- diminuiu 61% do tempo
+
+# 50x50
+# --- 11.123634099960327 seconds --- diminuiu 65% do tempo
+# --- 7.264829158782959 seconds ---
+
+# 100x100
+# --- 108.47347044944763 seconds --- 1min diminuiu 82% do tempo
+
+# 160x160
+# --- 1083.288479566574 seconds --- 18min diminuiu mais de 93% do tempo
+
+# 200x200
+# --- 2216.2952921390533 seconds --- 36min
+
+# 300x300
+# vai
+
+# 400x400
+# vai
+
+# 500x500
+# vai
+
+# 1000x1000
+# vai
+
+# 2000x2000
+# vai
+
+# 10000x10000
+# vai
+
+# 50000x50000
+# vai
+
+# 100000x100000
+# vai
+
+# 1000000x1000000
+# vai
+
+
+# ----------------------------------------------
+# NORMAL VALUE ITERATION RESULTS (OLD ALGORITHM):
+# ----------------------------------------------
+
+# 3x4
+# --- 0.0059969425201416016 seconds ---
+# --- 0.004996299743652344 seconds ---
+# --- 0.006995201110839844 seconds ---
+# --------------- 0,0059 average
+
+# 10x10
+# --- 0.07695412635803223 seconds ---
+# --- 0.06796026229858398 seconds ---
+# --- 0.07095956802368164 seconds ---
+# --------------- 0,0719 average
+
+# 30x30
+# --- 4.072670221328735 seconds ---
+
+# 40x40
+# --- 13.774938583374023 seconds ---
+
+# 50x50
+# --- 31.22613263130188 seconds ---
+
+# 100x100
+# --- 590.0848195552826 seconds --- 9min
+
+# 160x160
+# more than 4h15min
+
+# 170x170
+# memory error
+
+# 200x200
+# memory error
+
+# 300x300
+# memory error
+
+# 400x400
+# memory error
+
+# 500x500
+# memory error
+
+# 1000x1000
+# memory error
---
+++ gridworld_scenario.py
@@ -0,0 +1,305 @@
+import numpy as _np
+import math
+#import tensorflow as tf  # .\venv\Scripts\activate
+import scipy.sparse as _sp
+import sys
+
+sys.path.insert(1, 'pymdptoolbox/src')
+import mdptoolbox.example
+
+"""
+ACTIONS = ['N','S','E','W']
+input:
+shape -> [Y,X] shape of the grid
+obstacles -> [[Y1,X1], [Y2,X2]] list with the position of obstacles
+terminals -> [[Y1,X1], [Y2,X2]] list with the position of terminal states
+pm -> (0.0 - 1.0) the probability of successfully moving
+r -> (double/int value) the default reward for all states
+rewards -> [[Y1,X1,R1], [Y2,X2,R2]] a cell array of [Y,X,R] rewards for specific states
+
+output: 
+P = (A x S x S) the transition function
+R = (A x S x S) the reward function
+"""
+
+
+def mdp_grid(shape=[], obstacles=[], terminals=[], r=1, rewards=[], actions=[], final_limits=[]):
+    states = shape[0] * shape[1]  # 3x4
+    actions = actions
+    r = r
+    # P = _np.zeros([len(actions), states, states])
+    terminals = terminals
+    obstacles = obstacles
+    final_limits = final_limits  # 2x3
+
+    p_intended = 0.8  # probability of the desired action taking place
+    p_right_angles = (1 - p_intended) / 2  # 0.1 # stochasticity
+    p_opposite_angle = 0.0  # zero probability
+
+    # State Transition Probability Matrix
+    # N                E                 W                 S
+    STPM = [[p_intended, p_right_angles, p_right_angles, p_opposite_angle],  # North
+            [p_right_angles, p_intended, p_opposite_angle, p_right_angles],  # East
+            [p_right_angles, p_opposite_angle, p_intended, p_right_angles],  # West
+            [p_opposite_angle, p_right_angles, p_right_angles, p_intended]]  # South
+
+    # STPM = [[0.8(N,N), 0.1(N,E), 0.1(N,W), 0.0(N,S)],
+    #         [0.1(E,N), 0.8(E,E), 0.0(E,W), 0.1(E,S)],
+    #         [0.1(W,N), 0.0(W,E), 0.8(W,W), 0.1(W,S)],
+    #         [0.0(S,N), 0.1(S,E), 0.1(S,W), 0.8(S,S)]]
+
+    successors = _np.array([])
+    origins = _np.array([])
+    probabilities = _np.array([])
+
+    num_states = 1
+    for value in shape:
+        num_states *= value
+    #print(num_states)
+
+    for a in STPM:
+        for s in range(num_states):
+            #for d in range(len(shape)):
+            for aa in range(len(a)):  # 4
+                if a[aa] == 0:  # remove all zero probabilities
+                    continue
+
+                state_pair = ind2sub(shape, s)
+
+
+                #state_from = sub2ind(shape, x, y)
+
+                # xy = [x, y]
+                # successor = succ(aa, x, y, final_limits)
+                # print("a:", a, "aa:", a[aa], "index aa:", aa, " x:", x, " y:", y, " succ:", successor)
+
+                successor_i, successor_j = succ(aa, state_pair, final_limits)
+
+                # if valid(successor_i, successor_j, obstacles):
+                if [successor_i, successor_j] not in obstacles:
+                    state_to = sub2ind(shape, successor_i, successor_j)
+                    # P[STPM.index(a), state_from, state_to] = P[STPM.index(a), state_from, state_to] + a[aa]
+                    # successor = ind2sub(shape, state_to)
+                    # Ca = Ca + [xy, successor, a[aa]]
+
+                    if state_pair in terminals or state_pair in obstacles:
+                        #rank_3_tensor = tf.constant([
+                            #state_from,
+                            #state_from,
+                            #0.0])
+                        #output.append(rank_3_tensor)
+                        successors = _np.append(successors, s)
+                        origins = _np.append(origins, s)
+                        probabilities = _np.append(probabilities, 0)
+
+                    else:
+                        #rank_3_tensor = tf.constant([
+                            #state_from,
+                            #state_to,
+                            #a[aa]])
+                        #output.append(rank_3_tensor)
+                        successors = _np.append(successors, state_to)
+                        origins = _np.append(origins, s)
+                        probabilities = _np.append(probabilities, a[aa])
+
+                else:
+                    # P[STPM.index(a), state_from, state_from] = P[STPM.index(a), state_from, state_from] + a[aa]
+                    # Ca = Ca + [xy, xy, a[aa]]
+                    # successor = ind2sub(shape, state_from)
+
+                    #rank_3_tensor = tf.constant([
+                        #state_from,
+                        #state_from,
+                        #a[aa]])
+                    #output.append(rank_3_tensor)
+                    successors = _np.append(successors, s)
+                    origins = _np.append(origins, s)
+                    probabilities = _np.append(probabilities, a[aa])
+
+    # split the output into 4 arrays (because there are 4 actions)
+    #split = tf.split(output, len(STPM), axis=0, num=None, name='split')
+    #print(split)
+
+    successors = successors.astype(int)
+    succ_xy = _np.split(successors, len(STPM))
+    #print("succ", succ_xy)
+
+    origins = origins.astype(int)
+    origin_xy = _np.split(origins, len(STPM))
+    print("origin", origin_xy)
+    print(len(origin_xy[0]))
+    print(ind2sub(shape, origin_xy))
+
+    probability_xy = _np.split(probabilities, len(STPM))
+    #print("prob", probability_xy)
+
+    # convert all splits into a tensor
+    #split_tensor = tf.convert_to_tensor(list(split), dtype=tf.float32)
+    #split_tensor_origin_xy = tf.convert_to_tensor(list(origin_xy), dtype=tf.int32)
+    #split_tensor_succ_xy = tf.convert_to_tensor(list(succ_xy), dtype=tf.int32)
+
+    #dimensions = split_tensor.ndim
+
+    #print(split_tensor)
+    #print(succ_xy)
+
+    #print("Type of every element:", split_tensor.dtype)  # float32
+    #print("Number of dimensions:", split_tensor.ndim)  # 3
+    #print("Shape of tensor:", split_tensor.shape)  # (4, 27, 3)
+    #print("Elements along axis 0 of tensor:", split_tensor.shape[0])  # 4
+    #print("Elements along the last axis of tensor:", split_tensor.shape[-1])  # 3
+    #print("Total number of elements (3*2*4*5): ", tf.size(split_tensor).numpy())  # 324
+
+    R = _np.ones([states])
+    R = _np.multiply(R, r)
+
+    SR_output = []
+    for i in range(len(rewards)):
+        Si = rewards[i][0]  # 0 # 1
+        Sj = rewards[i][1]  # 3 # 3
+        Sv = rewards[i][2]  # 100 # -100
+        SR = sub2ind(shape, Si, Sj)  # index 3 and index 7
+        SR_output.append(SR)
+        R[SR] = Sv  # 100 # -100
+
+    # RSS = r_to_rs(P, R, terminals, shape)
+    # print(RSS)
+
+    #RW = reward_tensor(split_tensor, r, rewards, terminals, obstacles, shape)
+    # print(RW)
+
+    return succ_xy, origin_xy, probability_xy, R
+
+
+def reward_tensor(split_tensor, r, rewards, terminals, obstacles, shape):
+    RS_tensor = _np.zeros([shape[0], shape[1]])
+
+    for i in range(shape[0]):
+        for j in range(shape[1]):
+            if [i, j] in terminals:
+                RS_tensor[i, j] = rewards[i][2]
+
+            elif [i, j] in obstacles:
+                RS_tensor[i, j] += r
+
+            else:
+                for k in split_tensor[0]:
+                    pair_xy = ind2sub(shape, k[0])
+
+                    if [i, j] == pair_xy:
+                        RS_tensor[i, j] += k[2] * r
+
+    return RS_tensor
+
+
+"""
+def r_to_rss(P, R, terminals, obstacles):
+     RSS = _np.zeros([4,len(P[1]),len(P[1])])
+     for A in range(4):
+         for I in range(len(P[1])):
+             for J in range(len(P[1])):
+                 if([I,J] in terminals):
+                     RSS[A,I,J] = R[J]
+                 if([I,J] in obstacles): RSS[A,I,J] = 0
+                 RSS[A,I,J] = (P[A,I,J] * R[J])
+     return RSS
+
+
+def r_to_rs(P, R, terminals, shape):
+    RS = _np.zeros([len(P[1]), 4])
+    for I in range(len(P[1])):
+        for A in range(4):
+            sub = ind2sub(shape, I)
+            # if sub in obstacles: RS[I,A] = 0
+            if sub in terminals:
+                RS[I, A] = R[I]
+            else:
+                for J in range(len(P[1])):
+                    RS[I, A] = RS[I, A] + (P[A, I, J] * R[J])
+    return RS
+"""
+
+
+def sub2ind(shape, rows, cols):
+    return rows * shape[1] + cols
+
+
+def ind2sub(shape, ind):
+
+    rows = int((ind / shape[1]))
+    cols = int((ind % shape[1]))
+    return [rows, cols]
+
+
+"""
+def valid(x, y, obstacles):
+    # valid = ((I >= 0) and (I < shape[0])) and ((J >= 0) and (J < shape[1]))
+    valid = not [x, y] in obstacles
+    return valid
+"""
+
+
+def succ(a, state_pair, final_limits):
+    if a == 0:  # North
+        if state_pair[0] != 0:  # 1: limite inicial de X:0
+            D = [state_pair[0] - 1, state_pair[1]]
+        else:
+            D = [state_pair[0], state_pair[1]]
+
+    if a == 1:  # East
+        if state_pair[1] != final_limits[1]:  # 4: limite final de Y:3
+            D = [state_pair[0], state_pair[1] + 1]
+        else:
+            D = [state_pair[0], state_pair[1]]
+
+    if a == 2:  # West
+        if state_pair[1] != 0:  # 1: limite inicial de Y:0
+            D = [state_pair[0], state_pair[1] - 1]
+        else:
+            D = [state_pair[0], state_pair[1]]
+
+    if a == 3:  # South
+        if state_pair[0] != final_limits[0]:  # 3 limite inicial de X: 2
+            D = [state_pair[0] + 1, state_pair[1]]
+        else:
+            D = [state_pair[0], state_pair[1]]
+
+    return D[0], D[1]
+
+
+def print_policy(policy, shape, obstacles=[], terminals=[], actions=[]):
+    p_policy = _np.empty(shape, dtype=object)
+
+    for i in range(len(policy)):
+        sub = ind2sub(shape, i)
+        if sub in obstacles:
+            p_policy[sub[0]][sub[1]] = 'O'
+        elif sub in terminals:
+            p_policy[sub[0]][sub[1]] = 'T'
+        else:
+            p_policy[sub[0]][sub[1]] = actions[policy[i]]
+            #print(actions[policy[i]])
+    print(p_policy)
+
+
+from IPython.display import HTML, display
+
+SYMBOLS = ['&uarr;', '&darr;', '&rarr;', '&larr;']
+
+
+def display_policy(policy, shape, obstacles=[], terminals=[]):
+    p_policy = _np.empty(shape, dtype=object)
+    for i in range(len(policy)):
+        sub = ind2sub(shape, i)
+        if sub in obstacles:
+            p_policy[sub[0]][sub[1]] = '&#x25FE;'
+        elif sub in terminals:
+            p_policy[sub[0]][sub[1]] = '&#x25CE;'
+        else:
+            p_policy[sub[0]][sub[1]] = SYMBOLS[policy[i]]
+    display(HTML(
+        '<table style="font-size:300%;border: thick solid;"><tr>{}</tr></table>'.format(
+            '</tr><tr>'.join(
+                '<td>{}</td>'.format('</td><td>'.join(str(_) for _ in row)) for row in p_policy)
+        )
+    ))
