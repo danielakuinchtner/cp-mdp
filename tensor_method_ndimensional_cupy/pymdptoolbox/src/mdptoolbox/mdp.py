33dfd4084e1a5ff77d596893f716f861e3e48c73
danielakuinchtner <danielakuinchtner@gmail.com>
2020-10-23 09:50:07 -0300

    cupy

--- tensor_method_ndimensional_cupy/pymdptoolbox/src/mdptoolbox/mdp.py
+++ tensor_method_ndimensional_cupy/pymdptoolbox/src/mdptoolbox/mdp.py
@@ -60,8 +60,7 @@ import time as _time
 
 import numpy as _np
 import scipy.sparse as _sp
-
-
+import cupy as cp
 import mdptoolbox.util as _util
 
 _MSG_STOP_MAX_ITER = "Iterating stopped due to maximum number of iterations " \
